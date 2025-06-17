2c236df58f08c3d5583889c824965578d62cca00
jperdomounc <jcperdomo100@gmail.com>
2025-06-16 22:37:21 -0400

    fastapi and react

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,27 +1,40 @@
-"""read up on pydantic documentation"""
-"""--pip3 install fast api "uvicorn[standard]"--"""
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List, Optional
-import schedule_ranker
+from typing import List
+
+class Fruit(BaseModel):
+    name:str
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
 
 app = FastAPI()
 
-class Section(BaseModel):
-    start: float
-    end: float
-    prof_id: str
+origins = [
+    "http://localhost:5173"
+]
 
-class ScheduleRequest(BaseModel):
-    course_sections: List[List[Section]]
-    preferred_range: List[float]
-    prof_ratings: dict[str, float]
-
-@app.post("/generate_schedule/")
-def generate_schedule(request: ScheduleRequest):
-    schedules = generate_schedules(
-        schedule_range=tuple(request.preferred_range),
-        course_list=[[s.dict() for s in course] for course in request.course_sections],
-        prof_ratings=request.prof_ratings
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
     )
-    return {"schedules": schedules}
+
+memory_db = {"fruits": []}
+
+@app.get("/fruits", response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits = memory_db["fruits"])
+
+
+@app.post("/fruits")
+def add_fruits(fruit: Fruit):
+    memory_db["fruits"].append(fruit)
+    return fruit
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
